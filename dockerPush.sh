1a365a19317e837ac4f6813ee089ef3b01361511
emedayinc17 <emedayinc@gmail.com>
2025-09-30 20:15:54 -0500

    fix config.py-connectionstring v2

--- dockerPush.sh
+++ dockerPush.sh
@@ -1,6 +1,7 @@
 
 # en E:\eventos_peru  (raíz)
 docker build -t emeday17/eventos-iam:0.1.0         -f services/iam-service/Dockerfile .
+docker build -t emeday17/eventos-iam:latest        -f services/iam-service/Dockerfile .
 docker build -t emeday17/eventos-catalogo:0.1.0    -f services/catalogo-service/Dockerfile .
 docker build -t emeday17/eventos-proveedores:0.1.0 -f services/proveedores-service/Dockerfile .
 docker build -t emeday17/eventos-contratacion:0.1.0 -f services/contratacion-service/Dockerfile .
@@ -9,6 +10,7 @@ docker build -t emeday17/eventos-contratacion:0.1.0 -f services/contratacion-ser
 
 
 docker push emeday17/eventos-catalogo:0.1.0
+docker push emeday17/eventos-catalogo:latest
 docker push emeday17/eventos-iam:0.1.0
 docker push emeday17/eventos-contratacion:0.1.0
 docker push emeday17/eventos-proveedores:0.1.0
